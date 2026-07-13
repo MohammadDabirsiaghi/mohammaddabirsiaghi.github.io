b3c4e43810bf14ddf32320f3b825a93c6a6560f4
dabirsiaghim <dabirsiaghim@gmail.com>
2026-07-13 16:37:02 +0330

    54

--- assets/config.js
+++ assets/config.js
@@ -2,5 +2,5 @@
 
 window.APP_CONFIG = Object.freeze({
     API_URL:
-        "https://script.google.com/macros/s/AKfycbww4kme7L9P7VarLNxCQOsRbqq7bZQsemzVL90yHAcK2e70U_Z910y6-mRfM8UqxOIU/exec"
+        "https://script.google.com/macros/s/AKfycbzqjL52GJbqCeMaQvHunPu_TDLWYOCVpVnKixNQerrCaAmSlb3vFzFimbgYFNQqDYHW/exec"
 });
